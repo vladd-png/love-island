7badd6297e3f9c3e8eb1a9507d7a88fabd104c88
Virginia Ladd <virginialadd2@gmail.com>
2020-02-07 14:14:49 -0700

    Add an app test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,22 @@ import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+
+  it('should be an instance of App component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update its state when addContestant is called', () => {
+    const player = {id: 2, name: 'Ranger', status: 'Busy', interests: 'Peanut Butter and Squirrels' }
+    const expected = [
+      {id: 1, name: 'Leta', status: 'online', interests: 'Cats and Burritos' },
+      {id: 2, name: 'Ranger', status: 'Busy', interests: 'Peanut Butter and Squirrels' }
+  ]
+    const wrapper = shallow(<App />);
+    wrapper.instance().addContestant(player);
+    expect(wrapper.state('contestants')).toEqual(expected);
+  })
+
 });
